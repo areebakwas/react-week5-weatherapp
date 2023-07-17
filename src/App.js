c50e03b707f6f371d425d990008f23488becd4af
areebakwas <32884881+areebakwas@users.noreply.github.com>
2023-07-17 05:44:52 -0400

    Added link for github

--- src/App.js
+++ src/App.js
@@ -4,7 +4,13 @@ import './App.css';
 export default function App() {
   return (
     <div className="App">
-        <h1>Weather</h1>
+        <h1>Weather App</h1>
+      <footer>
+        This project was coded by Areeba A. and is {" "} 
+        <a href="https://github.com/areebakwas/react-week5-weatherapp" target="_blank" rel="noreferrer">
+           open-sourced on Github.
+        </a>
+      </footer>
     </div>
   );
 }

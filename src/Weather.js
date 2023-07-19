11a42ec1334af67f748899544a84d54296cf5335
areebakwas <32884881+areebakwas@users.noreply.github.com>
2023-07-19 05:55:37 -0400

    Fixed floating issue

--- src/Weather.js
+++ src/Weather.js
@@ -16,14 +16,12 @@ export default function Weather() {
             <li>Monday 6:00 AM</li>
             <li>Cloudy</li>
         </ul>
-        <div className="row">
+        <div className="row mt-3">
             <div className="col-6">
                 <div className="clearfix">
                 <img src="https://ssl.gstatic.com/onebox/weather/64/cloudy.png" alt="cloudy" className="float-left" />
-                <div className="float-left">
                 <span className="temperature">24</span>
                 <span className="unit">°C</span>
-                </div>
                 </div>
             </div>
             <div className="col-6">

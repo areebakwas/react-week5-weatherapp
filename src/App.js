7602af775a8b340b60f5114fd353d673016c7630
areebakwas <32884881+areebakwas@users.noreply.github.com>
2023-07-19 05:25:29 -0400

    Updated CSS

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
 
 import './App.css';
+import Weather from "./Weather";
 
 export default function App() {
   return (
     <div className="App">
-        <h1>Weather App</h1>
+      <div className="container">
+        
+        <Weather />
       <footer>
         This project was coded by Areeba A. and is {" "} 
         <a href="https://github.com/areebakwas/react-week5-weatherapp" target="_blank" rel="noreferrer">
            open-sourced on Github.
         </a>
       </footer>
+      </div>
     </div>
   );
 }
---
+++ src/Weather.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import "./Weather.css";
+
+
+export default function Weather() {
+    return (
+        <div className="Weather">
+        <form>
+            <div className="row">
+            <div className="col-9"><input type="search" placeholder="Enter a city..." className="form-control" autoFocus="on" /></div>
+            <div className="col-3"><input type="submit" value="Search" className="btn btn-primary w-100" /></div>
+            </div>
+        </form>
+        <h1>New York</h1>
+        <ul>
+            <li>Monday 6:00 AM</li>
+            <li>Cloudy</li>
+        </ul>
+        <div className="row">
+            <div className="col-6">
+                <div className="clearfix">
+                <img src="https://ssl.gstatic.com/onebox/weather/64/cloudy.png" alt="cloudy" className="float-left" />
+                <div className="float-left">
+                <span className="temperature">24</span>
+                <span className="unit">°C</span>
+                </div>
+                </div>
+            </div>
+            <div className="col-6">
+                <ul>
+                    <li>Precipitation: 3%</li>
+                    <li>Humidity: 93%</li>
+                    <li>Wind: 6 km/h</li>
+                </ul>
+            </div>
+        </div>
+            
+    </div>
+    )
+}
